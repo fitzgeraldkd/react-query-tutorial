9bc2eb34740da2aad67ecb5798203a8d4b9982e7
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2022-02-16 13:06:18 -0500

    Add query keys

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const res = await fetch('https://swapi.dev/api/planets/');
+const fetchPlanets = async ({ queryKey }) => {
+  const [_key, greeting, page] = queryKey;
+  const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 function Planets() {
-  const { data, status } = useQuery('planets', fetchPlanets, {
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(['planets', 'hello', page], fetchPlanets, {
     staleTime: 0,
     // cacheTime: 10,
     onSuccess: () => console.log('data fetched successfully'),
   });
-  console.log(data);
 
   return (
     <div>
       <h2>Planets</h2>
+
+      <button onClick={() => setPage(1)}>page 1</button>
+      <button onClick={() => setPage(2)}>page 2</button>
+      <button onClick={() => setPage(3)}>page 3</button>
       
       {status === 'loading' && (
         <div>Loading data...</div>
