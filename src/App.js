dd4040163b84d359ab8b650f3a62dc71e462feec
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2022-02-16 12:38:45 -0500

    Implement React Query for fetching data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Navbar from "./components/Navbar";
 import People from './components/People';
 import Planets from './components/Planets';
+
+const queryClient = new QueryClient();
 
 function App() {
   const [page, setPage] = useState('planets');
 
   return (
     <div className="App">
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className='content'>
-        { page === 'planets' ? <Planets /> : <People /> }
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className='content'>
+          { page === 'planets' ? <Planets /> : <People /> }
+        </div>
+      </QueryClientProvider>
     </div>
   );
 }
